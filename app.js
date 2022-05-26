9b68d5cddbb9dd71569c378a20f2c0a6da10971c
cganzon <crganzon@gmail.com>
2022-05-25 20:51:09 -0700

    create function the calls one of the simple math functions depending on the operator

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const add = (numOne, numTwo) => numOne + numTwo;
 const subtract = (numOne, numTwo) => numOne - numTwo;
 const multiply = (numOne, numTwo) => numOne * numTwo;
 const divide = (numOne, numTwo) => numOne / numTwo;
+
+const operate = (operator, numOne, numTwo) => {
+  if (operator === "+") return add(numOne, numTwo);
+  if (operator === "-") return subtract(numOne, numTwo);
+  if (operator === "*") return multiply(numOne, numTwo);
+  if (operator === "/") return divide(numOne, numTwo);
+};
