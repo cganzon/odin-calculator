eef2468339c5d65dc4d722fc1d80315179164988
cganzon <crganzon@gmail.com>
2022-05-25 20:46:52 -0700

    create simple math functions

---
+++ app.js
@@ -0,0 +1,4 @@
+const add = (numOne, numTwo) => numOne + numTwo;
+const subtract = (numOne, numTwo) => numOne - numTwo;
+const multiply = (numOne, numTwo) => numOne * numTwo;
+const divide = (numOne, numTwo) => numOne / numTwo;
