b772df9a36b742608e2b14ec906cd2913ac78364
cganzon <crganzon@gmail.com>
2022-05-26 19:18:33 -0700

    add event listeners to buttons

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const numberBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const decimalBtn = document.querySelector('#decimal');
+const equalsBtn = document.querySelector('#equals');
+const allClearBtn = document.querySelector('#all-clear');
+const deleteBtn = document.querySelector('#delete');
+
 const add = (numOne, numTwo) => numOne + numTwo;
 const subtract = (numOne, numTwo) => numOne - numTwo;
 const multiply = (numOne, numTwo) => numOne * numTwo;
@@ -9,3 +16,27 @@ const operate = (operator, numOne, numTwo) => {
   if (operator === "*") return multiply(numOne, numTwo);
   if (operator === "/") return divide(numOne, numTwo);
 };
+
+numberBtns.forEach(button => {
+  button.addEventListener('click', e => {
+    alert(e.target.textContent);
+  })
+})
+
+operatorBtns.forEach(button => {
+  button.addEventListener('click', e => {
+    alert(e.target.textContent);
+  })
+})
+
+decimalBtn.addEventListener('click', e => {
+  alert(e.target.textContent);
+})
+
+allClearBtn.addEventListener('click', e => {
+  alert(e.target.textContent);
+})
+
+deleteBtn.addEventListener('click', e => {
+  alert(e.target.textContent);
+})
